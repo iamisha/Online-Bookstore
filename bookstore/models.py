1885f5c9aa00128a0ac7f642ba9dbd2be9c5bcd6
iamisha <isha.201710@ncit.edu.np>
2024-01-01 21:42:26 +0545

    Book models created and implemented

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    GENRE_CHOICES = [
+        ('fiction', 'Fiction'),
+        ('non-fiction', 'Non-Fiction'),
+        ('mystery', 'Mystery'),
+        # Add more genres as needed
+    ]
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    cover_image = models.ImageField(upload_to='book_covers/')
+
+    def __str__(self):
+        return self.title
